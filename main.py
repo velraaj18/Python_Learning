e012f44a1a83186e80c5f5929aab182446efea73
Velraaj <velraaj30@gmail.com>
2026-06-29 10:36:03 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# first python program
+name = "Velraaj A S"
+age = 23
+city = "Madurai"
+profession = "Junior .NET developer"
+learning_python = True
+
+print(f"My name is {name}")
+print(f"I am {age} years old")
+print(f"I am living in {city}")
+print(f"I am currently working as a {profession}")
+print(f"Learning Python? {learning_python}")
+
